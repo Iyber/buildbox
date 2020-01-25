35705bd6d049b85a99f6eb620175e2a11c3a4446
Iyber <58360248+Iyber@users.noreply.github.com>
2020-01-26 03:05:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# form
+Nothing to read here. Just a custom made form for Buildbox 2.0 workshop. You're dree to use this stuff and make your own.
---
+++ app.js
@@ -0,0 +1,8 @@
+var form = document.querySelector('.pageclip-form')
+Pageclip.form(form, {
+  onSubmit: function (event) { },
+  onResponse: function (error, response) { },
+  successTemplate: '<img src = "yikKbjr4T.png" alt="Submitted"/><div>Hope to hear from us soon!</div>'
+})
+var formElement = document.querySelector('form')
+ValidForm(formElement)
